87d07ce0839b303304c5bb2d33548c66a1954f36
Oliver Davies <oliver@microserve.io>
2019-02-12 16:43:22 +0000

    Remove hard-coded hash salt

--- static-files/sites/default/settings.php
+++ static-files/sites/default/settings.php
@@ -774,5 +774,3 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-
-$settings['hash_salt'] = 'test';
